d0fa86aafa1688bf68ad1ca53a787061564444ef
R4F4I-NewPC <94185789+R4F4I@users.noreply.github.com>
2025-01-25 15:22:31 +0500

    initial commit

---
+++ OOP/OOP-LAB/assignments/lab-1/Q1_prime_check.cpp
@@ -0,0 +1,46 @@
+/*
+
+? |Write a C++ program to check whether a given number is prime or not. Allow the user to input a
+? |number and display whether it's prime or not?
+
+* Name:         Rafay Siddiqui
+* roll number:  24k-0009
+* section:      BAI-1A
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void printf(string str){
+    cout << str;
+}
+
+
+int main(){
+
+    int num,i;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    for (i = 2; i < num; i++)
+    {
+        if (num%i==0)
+        {
+            break;
+        }
+        
+    }
+    if (i==num){
+        printf("The number is prime\n");
+    }
+    else{
+        printf("The number is not prime\n");
+    }
+    
+
+
+    return 0;
+}
