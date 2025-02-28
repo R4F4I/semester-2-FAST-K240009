ddd15d29fd3a742a9ba400893f6e52ec18fba5c5
R4F4I <94185789+R4F4I@users.noreply.github.com>
2025-02-28 15:50:55 +0500

    q4

---
+++ OOP/OOP-LAB/assignments/lab-5/Q4.cpp
@@ -0,0 +1,66 @@
+/*
+
+? |
+Imagine Flight PK-303, a proud bird ready to soar from Karachi to England, dreaming of fish and
+chips.
+But plot twist—some unwanted guests (a.k.a. hijackers) decide to spice things up, forcing
+the plane to make an unplanned pit stop in Islamabad instead.
+Talk about a travel detour!
+Your job?
+Create a system that models this unexpected adventure using classes,
+showing the relationship (aggregation) between the flight and the airports.
+
+- The Flight class should be linked to multiple Airport objects
+        (because clearly, this flight loves collecting destinations).
+- Oh, and don’t forget a display() function
+    - to keep everyone updated—whether the plane is taking off, landing, or
+- stuck explaining things to airport security.
+
+(Now, fasten your seatbelt and start coding before this flight gets delayed even further!)
+
+* Name:         Rafay Siddiqui
+* roll number:  24k-0009
+* section:      BAI-2A
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Airports
+{
+private:
+    string name, place;
+
+public:
+    Airports(string name, string place)
+    {
+        this->name = name;
+        this->place = place;
+    }
+};
+
+class Flight
+{
+private:
+    Airports *origin, *destination;
+    string status;
+    int duration;
+
+public:
+    void display()
+    {
+
+        cout << this->status;
+    }
+    Flight(string *origin, string *destination, string status.)
+    {
+    }
+};
+int main()
+{
+
+    return 0;
+}
---
+++ OOP/OOP-LAB/assignments/lab-5/q6.cpp
@@ -0,0 +1,33 @@
+/*
+
+? |Say hello to your new financial sidekick, LoanHelper—the class that will kindly remind you how
+much you're paying every month for that loan you may or may not regret. It has a special variable
+to store the interest rate as a user defined constant value (but don’t go wild, it’s between 0% and
+0.5%—just enough to make you feel slightly guilty). You’ll give it your loan amount and the
+number of months you’re willing to spend thinking about your life choices. The process is simple:
+divide the loan by the months, add a sprinkle of interest, and boom! It’ll spit out something like,
+‘You have to pay 999 every month for 12 months to repay your loan.’ No more confusion—just
+pure, financial reality!""
+
+* Name:         Rafay Siddiqui
+* roll number:  24k-0009
+* section:      BAI-2A
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
